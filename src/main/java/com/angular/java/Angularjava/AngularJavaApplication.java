f8f48a3d5601a2ff3dab947c9c7d926926fc4f73
Swaroop Ray <Swaroop.Ray@wolterskluwer.com>
2018-08-14 15:55:40 +0530

    first commit

---
+++ src/main/java/com/angular/java/Angularjava/AngularJavaApplication.java
@@ -0,0 +1,12 @@
+package com.angular.java.Angularjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularJavaApplication.class, args);
+	}
+}
